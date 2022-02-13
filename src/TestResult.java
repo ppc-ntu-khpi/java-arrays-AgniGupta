c6fad35492636ab162160c6b4efec0403c4652e5
Mike <66411181+AgniGupta@users.noreply.github.com>
2022-02-13 13:39:36 +0000

    змінив

---
+++ src/TestResult.java
@@ -0,0 +1,12 @@
+//package test;
+
+//import domain.Exercise;
+
+public class TestResult {
+
+    public static void main(String[] args) {
+
+       int [] arr ={5,1,56,5};
+        System.out.println("Число яке повторюється найбільшу кількість разів " + Exercise.Calculate(arr));
+    }
+}
